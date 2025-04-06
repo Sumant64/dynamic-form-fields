39c49096c89a40b3956f7e3e2fa0590841fdd234
Sumant64 <sumant1995shah@gmail.com>
2025-04-06 19:45:38 +0530

    personal info pagination integrated

--- dynamic-form-field/app/components/personalInfo/PersonalInfoTable.jsx
+++ dynamic-form-field/app/components/personalInfo/PersonalInfoTable.jsx
@@ -1,4 +1,4 @@
-import { Paper, styled, Table, TableBody, TableCell, tableCellClasses, TableContainer, TableHead, TableRow, Tooltip } from "@mui/material";
+import { Paper, styled, Table, TableBody, TableCell, tableCellClasses, TableContainer, TableHead, TablePagination, TableRow, Tooltip } from "@mui/material";
 import React from "react";
 import SouthIcon from '@mui/icons-material/South';
 import NorthIcon from '@mui/icons-material/North';
@@ -6,11 +6,13 @@ import NorthIcon from '@mui/icons-material/North';
 const PersonalInfoTable = ({ columns, page, rowsPerPage, count, rows, setPage, setRowsPerPage, sort, setSort }) => {
   const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
-      backgroundColor: "#2C387E",
+      backgroundColor: '#1e6091',
       color: theme.palette.common.white,
+      padding: "5px",
     },
     [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
+      fontSize: 12,
+      padding: "5px",
     },
   }));
 
@@ -65,28 +67,6 @@ const PersonalInfoTable = ({ columns, page, rowsPerPage, count, rows, setPage, s
             </TableRow>
           </TableHead>
           <TableBody>
-            {/* {
-              rows?.length > 0 && rows.map((item, index) => {
-                let createDate = dateFormat(item.createdOn);
-                let scheduledDate = dateFormat(item.scheduled);
-
-
-                return (
-                  <StyledTableRow key={index}>
-                    {columns[columns.findIndex(item => item.field === 'ID' && item.display)] && <StyledTableCell>{item.id}</StyledTableCell>}
-                    {columns[columns.findIndex(item => item.field === 'Created On' && item.display)] && <StyledTableCell>{createDate}</StyledTableCell>}
-                    {columns[columns.findIndex(item => item.field === 'Payer' && item.display)] && <StyledTableCell>{item.payer}</StyledTableCell>}
-                    {columns[columns.findIndex(item => item.field === 'Status' && item.display)] && <StyledTableCell>{item.status}</StyledTableCell>}
-                    {columns[columns.findIndex(item => item.field === 'Email' && item.display)] && <StyledTableCell>{item.email}</StyledTableCell>}
-                    {columns[columns.findIndex(item => item.field === 'Payer Phone' && item.display)] && <StyledTableCell>{item.payerPhone}</StyledTableCell>}
-                    {columns[columns.findIndex(item => item.field === 'Services' && item.display)] && <StyledTableCell>{item.services}</StyledTableCell>}
-                    {columns[columns.findIndex(item => item.field === 'Scheduled' && item.display)] && <StyledTableCell>{scheduledDate}</StyledTableCell>}
-
-                  </StyledTableRow>
-                )
-              })
-            } */}
-
             {
                 rows?.length > 0 && rows.map((row, index) => {
                     console.log(row)
@@ -102,6 +82,35 @@ const PersonalInfoTable = ({ columns, page, rowsPerPage, count, rows, setPage, s
           </TableBody>
         </Table>
       </TableContainer>
+
+      {/* table pagination */}
+      {rows.length > 0 && (
+        <TablePagination
+          component={Paper}
+          elevation={2}
+          sx={{
+            "& .MuiTablePagination-selectLabel": {
+              margin: '0px'
+            },
+            "& .MuiTablePagination-displayedRows": {
+              margin: '0px'
+            },
+            "& .MuiTablePagination-input .MuiSvgIcon-fontSizeMedium": {
+              color: '#fff'
+            },
+            backgroundColor: "#1e6091",
+            padding: '0px !important',
+            color: "#fff",
+            bottom: "0px",
+            width: "100%",
+          }}
+          count={count}
+          page={page - 1}
+          onPageChange={(event, newPage) => setPage(newPage + 1)}
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={handleRowsPerPageChange}
+        />
+      )}
   </>
   );
 };
