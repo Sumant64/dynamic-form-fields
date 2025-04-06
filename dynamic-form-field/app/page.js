daba404f238ae85bb37d09aa1fef4e7e2c4a07ee
Sumant64 <sumant1995shah@gmail.com>
2025-04-06 19:57:16 +0530

    personal info table filter and search

--- dynamic-form-field/app/page.js
+++ dynamic-form-field/app/page.js
@@ -2,8 +2,10 @@
 import { useEffect, useState } from "react";
 import PersonalInfoTable from "./components/personalInfo/PersonalInfoTable";
 import { getConfigForm, getPersonalInfo } from "@/services/api";
-import { Button, Dialog } from "@mui/material";
+import { Box, Button, Dialog, InputBase, Paper } from "@mui/material";
 import ColumnFilterDialog from "./components/personalInfo/ColumnFilterDialog";
+import { CiFilter } from "react-icons/ci";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function Home() {
   const [rowsPerPage, setRowsPerPage] = useState(10);
@@ -79,6 +81,23 @@ export default function Home() {
     }
   };
 
+  const handleNoSearch = () => {
+    setSearchValue("");
+    initialLoad("", 1, 10);
+  };
+
+  const handleEnterPress = (e) => {
+    if (searchValue !== "" && e.key === "Enter") {
+      setPage(1);
+      setRowsPerPage(10);
+      initialLoad(searchValue, 1, 10);
+    }
+  };
+
+  const handleSearch = () => {
+    initialLoad(searchValue, 1, 10);
+  };
+
   return (
     <>
       {/* Columns filter dialog */}
@@ -89,21 +108,89 @@ export default function Home() {
           setOpenDialog={setOpenDialog}
         />
       </Dialog>
-      <Button onClick={() => setOpenDialog('filter')} variant="contained">Filter</Button>
 
-      {columns.length > 0 && (
-        <PersonalInfoTable
-          sort={sort}
-          setSort={setSort}
-          setRowsPerPage={setRowsPerPage}
-          page={page}
-          setPage={setPage}
-          columns={columns}
-          count={count}
-          rowsPerPage={rowsPerPage}
-          rows={rows}
-        />
-      )}
+      <Box paddingTop={"20px"}>
+        {/* Options Container */}
+        <Box
+          sx={{
+            display: "flex",
+            marginBottom: "10px",
+            justifyContent: "space-between",
+            flexWrap: "wrap",
+          }}
+        >
+          <Box
+            sx={{
+              display: "inline-flex",
+              border: '1px solid grey',
+              borderRadius: '10px'
+            }}
+          >
+            <Box
+              sx={{
+                cursor: "pointer",
+                padding: "4px 10px",
+              }}
+              onClick={() => setOpenDialog("filter")}
+            >
+              <CiFilter style={{position: 'relative', top: '20%'}} />
+            </Box>
+          </Box>
+
+          {/* Search Container */}
+          <Box
+            sx={{
+              position: "relative",
+              borderRadius: "10px",
+              backgroundColor: "#fffffff0",
+              border: `1px solid #e0e0e0`,
+              padding: "5px",
+              marginRight: "15px",
+              marginTop: { xs: "10px", md: "0px" },
+              width: { xs: "100%", md: "200px" },
+              height: "25px",
+              display: { xs: "flex", md: "flex" },
+            }}
+          >
+            <SearchIcon
+              sx={{ marginRight: "10px", cursor: "pointer" }}
+              onClick={handleSearch}
+            />
+            <InputBase
+              placeholder="Search Payer"
+              value={searchValue}
+              sx={{
+                height: "25px",
+                position: "relative",
+                width: '100%'
+              }}
+              inputProps={{ "aria-label": "search" }}
+              type="search"
+              onChange={(e) => {
+                setSearchValue(e.target.value);
+                if (e.target.value == "") {
+                  handleNoSearch();
+                }
+              }}
+              onKeyDown={(e) => handleEnterPress(e)}
+            />
+          </Box>
+        </Box>
+
+        {columns.length > 0 && (
+          <PersonalInfoTable
+            sort={sort}
+            setSort={setSort}
+            setRowsPerPage={setRowsPerPage}
+            page={page}
+            setPage={setPage}
+            columns={columns}
+            count={count}
+            rowsPerPage={rowsPerPage}
+            rows={rows}
+          />
+        )}
+      </Box>
     </>
   );
 }
